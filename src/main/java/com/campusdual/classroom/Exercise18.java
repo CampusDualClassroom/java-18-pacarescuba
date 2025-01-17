9cbc08633ac4cb02cf5617cb179efd3edc5d1f86
pacarescuba <94604499+pacarescuba@users.noreply.github.com>
2025-01-17 14:12:47 +0100

    Improved code robustness

--- src/main/java/com/campusdual/classroom/Exercise18.java
+++ src/main/java/com/campusdual/classroom/Exercise18.java
@@ -1,24 +1,30 @@
 package com.campusdual.classroom;
 
+import com.campusdual.util.Utils;
+
 public class Exercise18 {
 
-	public static void main(String[] args) {
-		showInlineArray(createAndInitializeArray(5));
-	}
+  public static void main(String[] args) {
+      showInlineArray(createAndInitializeArray(Utils.integer("Escriba la longitud del array: ")));
+  }
 
-	public static int[] createAndInitializeArray(int num) {
-		int[] intArray = new int[num];
-		for (int i = 0; i < num; i++){
-			intArray[i] = i + 1;
-		}
-		return intArray;
-	}
+  public static int[] createAndInitializeArray(int length){
+    if (length < 0) {
+      return new int[0];
+    } else {
+      int[] intArray = new int[length];
+      for (int i = 0; i < length; i++) {
+        intArray[i] = i + 1;
+      }
+      return intArray;
+    }
+  }
 
-	public static	void showInlineArray(int[] intArray) {
-		StringBuilder stringBuilder = new StringBuilder();
-		for (int i = 0; i < intArray.length; i++) {
-			stringBuilder.append((i + 1)).append(" ");
-		}
-		System.out.println(stringBuilder.toString().trim());
-	}
+  public static void showInlineArray(int[] intArray) {
+    StringBuilder stringBuilder = new StringBuilder();
+    for (int i = 0; i < intArray.length; i++) {
+      stringBuilder.append((i + 1)).append(" ");
+    }
+    System.out.println(stringBuilder.toString().trim());
+  }
 }
